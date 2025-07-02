441621c10030a38f4e530df7b93b66a516b00b51
shobhit5090 <sotyroxy00@gmail.com>
2025-07-02 18:15:04 +0530

    3 changes done

--- Project/src/com/flipfit/client/GymCustmerClient.java
+++ Project/src/com/flipfit/client/GymCustmerClient.java
@@ -9,7 +9,8 @@ package com.flipfit.client;
 public class GymCustmerClient {
     public void userMenu() {
         System.out.println("Good Morning Flipsters! ");
-        SYstem.out.println("Lets Start the work!! ");
+        System.out.println("Lets Start the work!! ");
+        System.out.println("Welcome to the Gym Flipsters! ");
     }
     public void handleUserActions() {}
 }
