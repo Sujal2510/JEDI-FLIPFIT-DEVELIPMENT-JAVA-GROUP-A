04c2fd89ee1963e0b3c2a56c6a40443c656f2e5e
shobhit5090 <sotyroxy00@gmail.com>
2025-07-02 17:41:40 +0530

    Ask to start work

--- Project/src/com/flipfit/client/GymCustmerClient.java
+++ Project/src/com/flipfit/client/GymCustmerClient.java
@@ -9,6 +9,7 @@ package com.flipfit.client;
 public class GymCustmerClient {
     public void userMenu() {
         System.out.println("Good Morning Flipsters! ");
+        SYstem.out.println("Lets Start the work!! ");
     }
     public void handleUserActions() {}
 }
